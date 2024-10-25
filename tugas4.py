0c823421eae59f89cc77c031ee1e67e78747fa18
Alfan jibal akbar <jibalalfan08@gmail.com>
2024-10-25 10:13:46 +0800

    Add files via upload

---
+++ tugas4.py
@@ -0,0 +1,10 @@
+print("nama saya adalah alfan jibal akbar")
+print("NIM saya 24241150")
+print("saya berasal dari gunungsari, lombok barat")
+print("A = nim pertama")
+print("B = nim terakhir")
+A = 24
+B = 50
+C = 74
+print("A+B=C")
+print(f"nilai dari C adalah :{A+B}" )
